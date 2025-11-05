70ea192bee9c590b8581a99533102e9b3084dc89
salomonmmunyaneza <munyanezajoel3@gmail.com>
2025-11-05 19:42:18 +0200

    new features

--- js-basics/intro.js
+++ js-basics/intro.js
@@ -1,22 +1,39 @@
-alert("Welcome to JavaScript Basics!");
-let age = 25
-let year = 2024
+//alert("Welcome to JavaScript Basics!");
+//let age = 25
+//let year = 2024
 
-console.log("age is", age);
-console.log("year is", year);
+//console.log("age is", age);
+//console.log("year is", year);
 
-const point = 100;
+//const point = 100;
 /* for the point = 150; This will cause an error
  because 'point' is a constant*/
-console.log('point is', point);
-var score = 170;
-console.log("score is", score);
+//console.log('point is', point);
+//var score = 170;
+//console.log("score is", score);
 /*different variables can be consoled together 
 when they are same data types*/
-console.log(age, year, point, score);
+//console.log(age, year, point, score);
 //constants and variables do not change value on next console//
-const min = 78
-min = 90;
-console.log("min is", min);
+//const min = 78
+//min = 90;
+//console.log("min is", min);
 var time = 50
 console.log("time is", time);
+//strings
+console.log("Hello World");
+
+//data types in JavaScript
+//string
+console.log("hello world");
+let email = 'munyaneza@gmail.com'
+console.log("email is", email);
+//string concatentation
+let firstName = 'munyaneza';
+let lastName = 'joel';
+let fullName = firstName + ' ' + lastName;
+console.log("full name is ", fullName);
+// extractin the characters in the string
+console.log(fullName[0]);
+//string length
+console.log(fullName.length);
