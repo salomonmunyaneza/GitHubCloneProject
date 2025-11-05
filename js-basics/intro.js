4cec9536b03cb68fbf8458d294fa51b57c74950f
salomonmmunyaneza <munyanezajoel3@gmail.com>
2025-11-05 14:57:39 +0200

    Add js-basics folder with intro.js

--- js-basics/intro.js
+++ js-basics/intro.js
@@ -1,6 +1,20 @@
-const name = "Amina";
+alert("Welcome to JavaScript Basics!");
+let age = 25
+let year = 2024
 
-// Print a greeting message
-console.log("Hello there!");
-console.log("Welcome to our program, " + name + "!");
-console.log("We hope you enjoy learning JavaScript!");
+console.log("age is", age);
+console.log("year is", year);
+
+const point = 100;
+/* for the point = 150; This will cause an error
+ because 'point' is a constant*/
+console.log('point is', point);
+var score = 170;
+console.log("score is", score);
+/*different variables can be consoled together 
+when they are same data types*/
+console.log(age, year, point, score);
+//constants and variables do not change value on next console//
+const min = 78
+min = 90;
+console.log("min is", min);
