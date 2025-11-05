5bfa6ec512e6ef96c5297a473be7bbdaef46024f
salomonmmunyaneza <munyanezajoel3@gmail.com>
2025-11-05 19:46:55 +0200

    data types

--- js-basics/intro.js
+++ js-basics/intro.js
@@ -37,3 +37,58 @@ console.log("full name is ", fullName);
 console.log(fullName[0]);
 //string length
 console.log(fullName.length);
+//string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result);
+//the methods do not alter the original string
+console.log(result, fullName);
+let index = email.indexOf('@');
+console.log("index of @ is", index);
+
+let results = email.lastIndexOf('n');
+console.log("the  position of last n is ", results);
+
+//slice method
+let portion = email.slice(0, 14);
+console.log("portion is", portion);
+//slicing using the substring method
+let part = email.substring(0, 7);
+console.log("part is", part);
+//we can use the replace method to replace a part of the string
+let newEmail = email.replace('munyaneza', 'salomon');
+console.log("new email is", newEmail);
+
+//numbers data types
+let radius = 12
+const pi = 3.14
+console.log(radius, pi);
+//mathematical operations +, -, *, /, **
+console.log(10 / 2);
+let remainder = 12 % 5;
+console.log("remainder is", remainder);
+
+let cirleArea = pi * radius ** 2;
+console.log("circle area is", cirleArea);
+//order of operations - BiDMAS
+let exercise = 5 * (10 - 3) ** 2;
+console.log("exercise is", exercise);
+//incrementing numbers
+let likes = 10;
+likes = likes + 1;
+console.log("likes are", likes);// and als++ act as +1
+likes++;
+console.log("likes after ++ are", likes);
+//the decrementing numbers
+likes--;
+console.log("likes after -- are", likes);
+
+likes += 10;
+console.log("likes after +=10 are", likes);
+likes -= 5;
+likes /= 2
+console.log("likes after -=5 and /=2 are", likes);
+//NaN - Not a Number results from invalid mathematical operations
+
+console.log("hello" - 3);
+console.log(8 / "hello");
